4952a33d747c340e8317eef66bfeeb1f36cfb79f
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2022-12-13 16:32:37 +0800

    Esercizio completato

---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "Jumbotron",
+    data() {
+        return {
+            img: "./public/TVD_cast.jpg"
+        }
+    }
+}
+</script>
+
+<template>
+    <img class="Jumbotron" :src="img" alt="">
+</template>
+
+<style>
+.jumbotron {
+    width: 100%;
+    height: 200px;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "Ciao",
+    data() {
+        return {
+            title: "La mia prima volta con Vite!"
+        }
+    }
+}
+</script>
+
+<template>
+    <h1> {{ title }}</h1>
+</template>
+
+<style>
+
+</style>
